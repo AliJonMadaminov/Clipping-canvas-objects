2391bcdaafa9221a268c82190709aa6dd8a94801
AliJonMadaminov <aliufc2004@gmail.com>
2022-11-02 16:17:02 +0500

    initial commit

---
+++ app/src/main/java/com/draw/paint/clippingcanvasobjects/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.draw.paint.clippingcanvasobjects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippedView(this))
+    }
+}
